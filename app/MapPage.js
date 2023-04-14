7296b75c4eda45c2adcd3f353952255e66a17d75
Shahed Mahamud Lemon <47500806+ShahedMahamudLemon@users.noreply.github.com>
2023-04-14 23:57:04 +0600

    Project setup with navigation and map integration
    Project setup with navigation and map integration

---
+++ app/MapPage.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, View } from "react-native";
+const MapPage = () => {
+  const [mapRegion, setMapRegio] = useState({
+    latitude: 23.811056,
+    longitude: 90.407608,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  return (
+    <View style={styles.container}>
+      <MapView region={mapRegion} style={styles.map}>
+        <Marker coordinate={mapRegion} title="marker"></Marker>
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    marginTop: 35,
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default MapPage;
---
+++ app/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Link } from "expo-router";
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.baseText}>
+        Welcome to
+        <Text style={styles.innerText}> Smart-City</Text>
+      </Text>
+      <Link style={styles.btn} href="/MapPage">
+        Goto Map
+      </Link>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    marginTop: 100,
+    padding: 50,
+  },
+  baseText: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  innerText: {
+    color: "red",
+    fontSize: 30,
+  },
+  btn: {
+    marginTop: 15,
+    borderRadius: 10,
+    borderColor: "black",
+    backgroundColor: "black",
+    color: "white",
+    padding: 12,
+    fontSize: 20,
+  },
+});
+export default index;
